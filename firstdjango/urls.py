4770b46d168e715c3ab6f8368bbc815d1f48ebe5
swissify221 <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2024-10-28 17:10:15 +0600

    layout

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -7,5 +7,7 @@ from home.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('services/', services, name='services'),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'index.html', {'name': 'John'})
+    peopol =[
+        {'name': 'John', 'age': 25},
+        {'name': 'Jane', 'age': 24},
+        {'name': 'Joe', 'age': 26},
+        {'name': 'Jill', 'age': 27},
+        {'name': 'Jack', 'age': 28},
+    ]
+    page = "Home Page"
+    return render(request, 'index.html', context={'peopol': peopol, 'page': page})
 
 def about(request):
-    return HttpResponse("About Page")
+    page = "About Page"
+    return render(request, 'about.html', context={'page': page})
+
+def contact(request):
+    page = "Contact Page"
+    return render(request, 'contact.html',context={'page': page})
+
+def services(request):
+    page = "Services Page"
+    return render(request, 'services.html',context={'page': page})
