58e1ba779bea37875b951d34c18d8317dc17ede7
swissify221 <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2024-10-28 17:57:00 +0600

    migratation

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
+    addres = models.TextField(null=True)
+    # image = models.ImageField()
+    file = models.FileField()
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
